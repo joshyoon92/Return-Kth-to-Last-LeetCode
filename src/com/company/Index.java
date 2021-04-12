71d18f7898754065e0de3c6c00e8c6d9aaf07e51
Joshua Yoon <joshyoon92@gmail.com>
2021-04-12 16:44:35 +0900

    Initial commit

---
+++ src/com/company/Index.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class Index {
+    public int value = 0;
+
+    public LinkedListNode kthToLast(LinkedListNode head, int k){
+        Index idx = new Index();
+        return kthToLast(head,k,idx);
+    }
+
+    public LinkedListNode kthToLast(LinkedListNode head, int k, Index idx){
+        if (head ==null){
+            return null;
+        }
+        LinkedListNode node = kthToLast(head.next, k, idx);
+        idx.value = idx.value +1;
+        if (idx.value ==k){
+            return head;
+        }
+        return node;
+    }
+}
